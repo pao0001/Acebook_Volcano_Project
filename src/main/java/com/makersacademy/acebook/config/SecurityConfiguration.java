7304a8282e5110237d4b47658378992c79bc92bb
zehad <zehad.tech@gmail.com>
2025-06-04 16:51:16 +0100

    adding welcome page

--- src/main/java/com/makersacademy/acebook/config/SecurityConfiguration.java
+++ src/main/java/com/makersacademy/acebook/config/SecurityConfiguration.java
@@ -1,5 +1,5 @@
 package com.makersacademy.acebook.config;
-
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -33,9 +33,12 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/images/**").permitAll()
+                        .requestMatchers("/welcome", "/images/**").permitAll()
                         .anyRequest().authenticated()
+
                 )
+
+
                 .oauth2Login(oauth2 -> oauth2
                         .successHandler(new AuthenticationSuccessHandler() {
                             @Override
@@ -46,15 +49,16 @@ public class SecurityConfiguration {
                 )
 
                 .logout(logout -> logout
-                        .addLogoutHandler(logoutHandler()));
+                        .addLogoutHandler(logoutHandler())
+                        .logoutSuccessUrl("/welcome")); // :point_left: Redirect target after logout);
         return http.build();
     }
 
     private LogoutHandler logoutHandler() {
         return (request, response, authentication) -> {
             try {
-                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
-                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + baseUrl);
+//                String baseUrl = ServletUriComponentsBuilder.fromCurrentContextPath().build().toUriString();
+                response.sendRedirect(issuer + "v2/logout?client_id=" + clientId + "&returnTo=" + "http://localhost:8080/welcome");
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
