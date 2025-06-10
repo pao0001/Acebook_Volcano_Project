7b205d12c12cf49ee38aee1300bce5d4411d1083
Alexandre Barrymore <alexandrebarrymore@MASTUDENT-Alexandre-Barrymore-THFD966NGP.local>
2025-06-10 15:08:25 +0100

    homepage, added famous pfp's, V16_migration, .gitignore edited

---
+++ src/main/resources/db/migration/V16__populate_famous_user_profile_images.sql
@@ -0,0 +1,8 @@
+UPDATE users SET profile_image_src = '/uploads/profile/taylor_swift.jpg' WHERE username = 't.swiftie89@popvibes.com';
+UPDATE users SET profile_image_src = '/uploads/profile/robertdj.jpg' WHERE username = 'robert.downey@starkmail.com';
+UPDATE users SET profile_image_src = '/uploads/profile/beyonce.jpg' WHERE username = 'bey.queenb@lemonadehub.net';
+UPDATE users SET profile_image_src = '/uploads/profile/therock.jpg' WHERE username = 'the.rock@smashmail.org';
+UPDATE users SET profile_image_src = '/uploads/profile/emmawatson.jpg' WHERE username = 'emma.watson.books@hermione.io';
+UPDATE users SET profile_image_src = '/uploads/profile/drake.jpg' WHERE username = 'drake@octobersveryown.ca';
+UPDATE users SET profile_image_src = '/uploads/profile/rihanna.jpg' WHERE username = 'rih.navy@fentyhq.co';
+UPDATE users SET profile_image_src = '/uploads/profile/zendaya.jpg' WHERE username = 'zendaya.now@webslings.tv';
